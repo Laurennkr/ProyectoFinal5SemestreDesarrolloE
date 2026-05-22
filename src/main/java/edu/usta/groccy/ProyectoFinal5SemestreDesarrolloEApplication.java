45353689caac255a122a358c70f655d2af74169b
Lauren <164786167+Laurennkr@users.noreply.github.com>
2026-05-21 20:43:17 -0500

    chore: create initial Spring Boot project

---
+++ src/main/java/edu/usta/groccy/ProyectoFinal5SemestreDesarrolloEApplication.java
@@ -0,0 +1,13 @@
+package edu.usta.groccy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinal5SemestreDesarrolloEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoFinal5SemestreDesarrolloEApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/usta/groccy/ProyectoFinal5SemestreDesarrolloEApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usta.groccy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinal5SemestreDesarrolloEApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
